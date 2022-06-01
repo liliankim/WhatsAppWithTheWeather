bf2b9a824e97df6ce0122c161d20af2ca8662843
liliankim <yejean831@gmail.com>
2022-06-01 09:13:46 -0700

    added weather API

---
+++ script.js
@@ -0,0 +1,7 @@
+var getresults = function (user) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={48cae7068ade684341af125894a1d181}" ;
+
+}
+
+console.log(getresults);
+
